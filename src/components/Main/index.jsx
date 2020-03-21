c410fa4f7eea97955c2b755bb9d432b715d0f81f
Aman Riat <amanriat777@gmail.com>
2020-03-21 14:21:59 -0400

    initial commit

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import styles from "./main.module.css";
+
+const Main = () => {
+  return(
+    <main id="mainContent">
+      <div className={styles.main}>
+        <p>This is placeholder text. Your web app description goes here.</p>
+        <a
+          href="https://github.com/Microsoft/WebTemplateStudio"
+          className="btn btn-primary my-2"
+        >
+          Link to our Github
+        </a>
+       </div>
+    </main>
+  );
+}
+export default Main;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,26 @@
+﻿import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./navbar.module.css";
+
+//TODO Web Template Studio: Add a new link in the NavBar for your page here.
+// A skip link is included as an accessibility best practice. For more information visit https://www.w3.org/WAI/WCAG21/Techniques/general/G1.
+const NavBar = () => {
+  return (
+    <React.Fragment>
+      <nav className={styles.nav}>
+        <div>
+          <Link className="navbar-brand" to="/">
+            NBA All Defense Classifier
+          </Link>
+        </div>
+        <div className={styles.date_text}>
+          <b>Aman Riat</b>
+        </div>
+        <div> 
+          <p>light and dark mode</p>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+}
+export default NavBar;
